b361b8a3706f9fbc1f30fa9ee6decb40c237f819
user <user@DESKTOP-B84F6RM>
2024-12-05 10:19:17 +0700

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+using UserLoginForm.Models;
+
+namespace UserLoginForm.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "invalid login attemp.");
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+
+
+       
+    }
+}
---
+++ Data/DbInitializer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UserLoginForm.Models;
+
+namespace UserLoginForm.Data
+{
+    public class DbInitializer
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider, UserManager<ApplicationUser> userManager)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                context.Database.EnsureCreated();
+
+                // Look for any users.
+                if (context.Users.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                var user = new ApplicationUser
+                {
+                    UserName = "huong@nuong.com",
+                    Email = "huong@nuong.com",
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(user, "1234");
+
+                var admin = new ApplicationUser
+                {
+                    UserName = "admin@example.com",
+                    Email = "admin@example.com",
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(admin, "Admin@1234");
+            }
+        }
+    }
+}
